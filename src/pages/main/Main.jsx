61865b5bfe7cdae94b0c0c3613b05e0f21c888cc
심서현 <simseohyun@simseohyeon-ui-MacBookAir.local>
2023-11-10 00:56:13 +0900

    :lipstick: Style: Main Style Fix

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import styled, { keyframes } from "styled-components";
 import { BsGithub, BsInstagram } from "react-icons/bs";
+import Button from "../../components/common/button/Button";
 
 const floatingAnimation = keyframes`
   0% {
@@ -19,10 +20,10 @@ const floatingAnimation = keyframes`
 const FloatingText = styled.div`
   font-size: 15px;
   font-weight: 600;
-  height: 10vh;
+  padding: 40px 0;
+
   display: flex;
   justify-content: center;
-  align-items: end;
   animation: ${floatingAnimation} 2s linear infinite;
 `;
 
@@ -47,122 +48,88 @@ function Main() {
 
   return (
     <S.MainWrapper>
-      <Link to={`userBlackBoard/test-url`}>칠판 바로가기</Link>
-      <br />
-      <Link to={`titleCreate`}>칠판 만들기</Link>
-      <br />
-      <Link to={`titleEditor`}>칠판 꾸미기 바로가기</Link>
-      <br />
-      <Link to={`letterCreate/test-url`}>편지 쓰기 바로가기</Link>
-      <br />
-      <Link to={`letterEditor/test-url`}>편지 꾸미기 바로가기</Link>
-      <br />
-      <Link to={`visitors/test-url`}>편지쓴명단 바로가기</Link>
-      <FloatingText>{displayedText}</FloatingText>
-      <S.MainBoardnum2>
-        <S.MainBoardnum2logo>
-          <img
-            src="/MainLogo.png"
-            alt="Step2_1"
-            width={"100%"}
-            height={"100%"}
-          />
-        </S.MainBoardnum2logo>
-      </S.MainBoardnum2>
-      <S.MainBoardnum3>
-        <S.MainBoardnum3crtbrdwrp>
-          <Link to="/titleCreate/1" style={{ width: "100%", height: "100%" }}>
-            <S.MainBoardnum3createboard>
-              내 칠판 만들기
-            </S.MainBoardnum3createboard>
-          </Link>
-        </S.MainBoardnum3crtbrdwrp>
+      <S.TitleWrapper>
+        <FloatingText>{displayedText}</FloatingText>
+        <S.LogoWrapper src="/MainLogo.png" />
+        <Link to={"/titleCreate"}>
+          <Button content={"내 칠판 만들기"} type={"white"} />
+        </Link>
 
-        <S.MainBoardnum3tu />
-      </S.MainBoardnum3>
-      <S.MainIntro>
-        <S.MainIntrobold>그때 그 시절, 칠판 편지</S.MainIntrobold>
-        <br />
-        <br />
-        초등학교,중학교,고등학교 졸업식,
-        <br />
-        반 친구들이 함께 모여 꾸미던 칠판 편지!
-        <br />
-        <br />
-        그때의 감성을 살린,온라인 칠판 편지입니다!
-      </S.MainIntro>
-      <S.Mainstep1>
-        <S.Mainstep1wrp>
-          <S.MainIntrogreencolor>Step 1</S.MainIntrogreencolor>
+        <div
+          style={{
+            backgroundColor: "#715948",
+            width: "100%",
+            height: "20px",
+            marginTop: "20px"
+          }}
+        />
+      </S.TitleWrapper>
+
+      <S.ContentWhiteWrapper>
+        <S.ContentTitle>그때 그 시절 칠판 편지</S.ContentTitle>
+        <S.ContentContent>
+          초등학교, 중학교, 고등학교 졸업식.
+          <br /> 반 친구들이 함께 모여 꾸미던 칠판 편지!
+          <br /> <br />
+          그때의 감성을 살린, 온라인 칠판 편지입니다!
           <br />
-          <S.MainIntrobold>칠판 등록</S.MainIntrobold>
+        </S.ContentContent>
+      </S.ContentWhiteWrapper>
+
+      <S.ContentGreyList>
+        <S.ContentGreyWrapper>
+          <S.ContentStep>Step 1</S.ContentStep>
+          <S.ContentTitle>칠판 등록</S.ContentTitle>
+          <S.ContentContent>
+            함께 꾸며나갈 칠판을 등록해주세요.
+            <br />
+            이때, 칠판의 주소가 어디인지 까먹지 않도록
+            <br />
+            이메일을 보내드립니다!
+          </S.ContentContent>
+          <S.ContentImg src="/Step1.png" />
+        </S.ContentGreyWrapper>
+
+        <S.ContentGreyWrapper>
+          <S.ContentStep>Step 2</S.ContentStep>
+          <S.ContentTitle>칠판 편지 작성</S.ContentTitle>
+          <S.ContentContent>
+            칠판에 끝맺음을 축하하고, 시작을 응원하는
+            <br />
+            따뜻한 한마디를 작성해주세요!
+            <br />
+            작성한 칠판 편지를 스티커로 꾸밀 수 있답니다.
+          </S.ContentContent>
+          <S.ContentImg src="/Step2.png" />
+        </S.ContentGreyWrapper>
+
+        <S.ContentGreyWrapper>
+          <S.ContentStep>Step 3</S.ContentStep>
+          <S.ContentTitle>디데이 카운트</S.ContentTitle>
+          <S.ContentContent>
+            사전에 지정한 날짜에 칠판편지가 공개됩니다!
+            <br />
+            디데이를, 칠판에 다녀간 사람들을 확인할 수 있어요.
+            <br />
+            기분 좋은 졸업식 되세요!
+          </S.ContentContent>
+          <S.ContentImg src="/Step3.png" />
+        </S.ContentGreyWrapper>
+      </S.ContentGreyList>
+
+      <S.FooterWrapper>
+        <S.FooterTitle>About US</S.FooterTitle>
+        <S.FooterContent>
+          졸축위 - 졸업 축하위원회
           <br />
-          <br />
-          함께 꾸며나갈 칠판을 등록해주세요.
-          <br />
-          이때, 칠판의 주소가 어디인지 까먹지 않도록
-          <br />
-          이메일을 보내드립니다!
-        </S.Mainstep1wrp>
-        <S.Mainstep1image>
-          <img src="/Step1.png" alt="Step1" width={"32%"} height={"60%"} />
-        </S.Mainstep1image>
-      </S.Mainstep1>
-      <S.Mainstep2>
-        <S.Mainstep2wrp>
-          <S.MainIntrogreencolor>Step 2</S.MainIntrogreencolor>
-          <br />
-          <S.MainIntrobold>칠판 편지 작성</S.MainIntrobold>
-          <br />
-          <br />
-          칠판에 끝맺음을 축하하고,시작을 응원하는
-          <br />
-          따듯한 한마디를 작성해주세요!
-          <br />
-          작성한 칠판 편지를 스티커로 꾸밀 수 있답니다.
-        </S.Mainstep2wrp>
-        <S.Mainstep2image>
-          <img src="/Step2_1.png" alt="Step2_1" width={"32%"} height={"55%;"} />
-          <img src="/Step2_2.png" alt="Step2_2" width={"32%"} height={"55%;"} />
-        </S.Mainstep2image>
-      </S.Mainstep2>
-      <S.Mainstep3>
-        <S.Mainstep3wrp>
-          <S.MainIntrogreencolor>Step 3</S.MainIntrogreencolor>
-          <br />
-          <S.MainIntrobold>디데이 카운트</S.MainIntrobold>
-          <br />
-          <br />
-          사전에 지정한 날짜에 칠판편지가 공개됩니다!
-          <br />
-          디데이를, 칠판에 다녀간 사람들을 확인할 수 있어요.
-          <br />
-          기분 좋은 졸업식 되세요!
-        </S.Mainstep3wrp>
-        <S.Mainstep3image>
-          <img src="/Step3_1.png" alt="Step3_1" width={"32%"} height={"65%;"} />
-          <img src="/Step3_2.png" alt="Step3_2" width={"32%"} height={"65%;"} />
-        </S.Mainstep3image>
-        <S.Mainstep3gocreate>
-          <Link to="/titleCreate/1">지금 바로 칠판을 만들어보세요! {">"}</Link>
-        </S.Mainstep3gocreate>
-      </S.Mainstep3>
-      <S.Mainaboutus>
-        <S.Mainaboutuswrp>
-          <S.MainIntrobold>About US</S.MainIntrobold>
-          <br />
-          <br />
-          졸축위-졸업 축하위원회{")"}
-          <br />
-          심서현 강민지 곽호은 김동환 이가현
-          <br />
-          <br />
-          <S.MainAboutLogo>
-            <BsInstagram />
-            <BsGithub />
-          </S.MainAboutLogo>
-        </S.Mainaboutuswrp>
-      </S.Mainaboutus>
+          심서현 강민서 곽호은 김동환 이가현
+        </S.FooterContent>
+
+        <S.FooterIconList>
+          <S.FooterIcon src="/Icon_Insta.svg" />
+          <S.FooterIcon src="/Icon_GIt.svg" />
+        </S.FooterIconList>
+      </S.FooterWrapper>
     </S.MainWrapper>
   );
 }
--- src/pages/main/style.jsx
+++ src/pages/main/style.jsx
@@ -2,254 +2,104 @@ import { styled } from "styled-components";
 
 export const MainWrapper = styled.div`
   width: 100%;
-`;
-
-// export const MainBoardnum = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   align-items: end;
-//   font-size: 15px;
-//   height: 20vh;
-//   font-weight: 600;
-//   background-color: blue;
-// `;
-
-export const MainBoardnum2 = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 50vh;
-`;
-export const MainBoardnum2logo = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-`;
-export const MainBoardnum2logo1 = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  height: 50%;
-  font-size: 25px;
-`;
-export const MainBoardnum2logo2 = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  height: 50%;
-  font-size: 14px;
-`;
-
-export const MainBoardnum3 = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  height: 12vh;
-`;
-export const MainBoardnum3crtbrdwrp = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   background-color: white;
-  width: 80%;
-  border-radius: 10px;
-  height: 55%;
-  color: #345342;
-  font-size: 20px;
-`;
-export const MainBoardnum3createboard = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  background-color: white;
-  border-radius: 10px;
-  height: 100%;
-  color: #345342;
-  font-size: 20px;
-  transition: background-color 0.3s, transform 0.2s;
-
-  &:hover {
-    background-color: white;
-    color: #345342;
-    transform: scale(1.05);
-  }
-`;
-export const MainBoardnum3tu = styled.div`
-  width: 100%;
-  height: 20%;
-  background-color: #715948;
 `;
 
-// export const MainNavWrapper = styled.div`
-//   height: 100%;
-//   display: flex;
-//   flex-direction: column;
-// `;
-// export const MainNavBtn = styled.div`
-//   padding: 5px 10px;
-//   font-size: 16px;
-// `;
-export const MainIntro = styled.div`
+export const TitleWrapper = styled.div`
+  padding-top: 20px;
+  background-color: ${props => props.theme.colors.main1};
+`;
+
+export const LogoWrapper = styled.img`
+  width: 100%;
+  padding-top: 40px;
+  padding-bottom: 80px;
+`;
+
+export const ContentWhiteWrapper = styled.div`
+  padding: 80px 0;
+  font-family: "Pretendard";
   display: flex;
   flex-direction: column;
-  font-family: "Pretendard-Regular", sans-serif;
+  justify-content: center;
+  align-items: center;
+
+  background-color: white;
+`;
+
+export const ContentGreyList = styled.div`
+  padding: 40px 0;
+  font-family: "Pretendard";
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  background-color: #f6f6f6;
+`;
+
+export const ContentGreyWrapper = styled.div`
+  padding: 20px 0;
+  font-family: "Pretendard-Regular";
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  background-color: #f6f6f6;
+`;
+
+export const ContentStep = styled.div`
+  font-size: 10px;
+  margin-bottom: 5px;
   font-weight: 700;
-  justify-content: center;
-  align-items: center;
-  color: #626262;
-  text-align: center;
-  width: 100%;
-  height: 30vh;
-  background-color: white;
+  color: ${props => props.theme.colors.grey1};
+`;
+export const ContentTitle = styled.div`
   font-size: 14px;
+  padding-bottom: 15px;
+  font-weight: 700;
+  color: #000000;
+`;
+export const ContentContent = styled.div`
+  width: 100%;
+  text-align: center;
+  font-size: 12px;
+  color: #626262;
   line-height: 1.2;
 `;
-export const MainIntrobold = styled.div`
-  font-weight: 700;
-  font-size: 17.5px;
-  color: black;
-`;
-export const MainIntrogreencolor = styled.div`
-  color: #486455;
+export const ContentImg = styled.img`
+  width: 100%;
+  padding: 0px 15px;
 `;
 
-export const Mainstep1 = styled.div`
+export const FooterWrapper = styled.div`
+  padding: 40px 15px;
   display: flex;
-  justify-content: center;
   flex-direction: column;
-  line-height: 1.1;
-  font-family: "Pretendard-Regular", sans-serif;
-  font-weight: 700;
-  align-items: center;
-  width: 100%;
-  height: 90vh;
-  background-color: #f6f6f6;
-  color: #626262;
+  font-family: "Pretendard-Regular";
 `;
-export const Mainstep1wrp = styled.div`
-  display: flex;
-  justify-content: end;
-  flex-direction: column;
-  text-align: center;
-  align-items: center;
-  height: 45%;
-  width: 100%;
-`;
-export const Mainstep2wrp = styled.div`
-  display: flex;
-  justify-content: end;
-  flex-direction: column;
-  text-align: center;
-  align-items: center;
-  height: 30%;
-  width: 100%;
-`;
-export const Mainstep3wrp = styled.div`
-  display: flex;
-  justify-content: end;
-  flex-direction: column;
-  text-align: center;
 
-  align-items: center;
-  height: 30%;
-  color: #848484;
-  width: 100%;
-`;
-export const Mainstep1image = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 70%;
-`;
-export const Mainstep2image = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 70%;
-  gap: 20px;
-`;
-export const Mainstep3image = styled.div`
-  width: 100%;
-  gap: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 50%;
-`;
-export const Mainstep3gocreate = styled.div`
-  width: 100%;
-  height: 5%;
-  display: flex;
-  justify-content: center;
-  align-items: end;
-  text-align: center;
-  color: #848484;
-  font-size: 17px;
-`;
-export const Mainstep2 = styled.div`
-  width: 100%;
-  height: 90vh;
-  background-color: #f6f6f6;
-  line-height: 1.1;
-  font-family: "Pretendard-Regular", sans-serif;
-  font-weight: 700;
-  color: #486455;
-`;
-export const Mainstep3 = styled.div`
-  width: 100%;
-  height: 100vh;
-  line-height: 1.1;
-  background-color: #f6f6f6;
-  font-family: "Pretendard-Regular", sans-serif;
-  font-weight: 700;
-  color: #486455;
-`;
-export const Mainaboutus = styled.div`
-  width: 100%;
-  font-family: "Pretendard-Regular", sans-serif;
-  font-weight: 700;
-  color: #486455;
-  height: 25vh;
-  flex-direction: column;
-  display: flex;
-  font-weight: 700;
-  justify-content: center;
-  align-items: center;
-  background-color: white;
-  color: black;
-`;
-export const Mainaboutuswrp = styled.div`
-  width: 90%;
-  height: 60%;
-  display: flex;
-  line-height: 1.1;
-  color: #626262;
-  justify-content: start;
-  flex-direction: column;
-  align-items: start;
-  background-color: white;
+export const FooterTitle = styled.div`
   font-size: 14px;
+  padding-bottom: 10px;
+  color: #000000;
+  font-weight: 700;
 `;
-export const MainAboutLogo = styled.div`
-  width: 100%;
-  height: 30%;
-  color: #e9e9e9;
+export const FooterContent = styled.div`
+  font-size: 12px;
+  padding-bottom: 10px;
+  color: #626262;
+  line-height: 1.2;
+`;
+
+export const FooterIconList = styled.div`
   display: flex;
-  font-size: 20px;
-  gap: 10px;
-  justify-content: start;
-  align-items: center;
+  margin-top: 10px;
+`;
+export const FooterIcon = styled.img`
+  width: 24px;
+  margin-right: 10px;
 `;
