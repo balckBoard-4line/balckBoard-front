d97f93129c2d5b6834c434dce28e74afc19fe272
심서현 <simseohyun@simseohyeon-ui-MacBookAir.local>
2023-11-10 20:12:05 +0900

    :sparkles: Feat: API

--- src/components/userBlackBoard/BeforeLaunch.jsx
+++ src/components/userBlackBoard/BeforeLaunch.jsx
@@ -28,6 +28,7 @@ function BeforeLaunch({ data, getIsLaunch }) {
         clearInterval(interval);
         setTimeLeft({ days: 0, hours: 0, minutes: 0, seconds: 0 });
       } else {
+        console.log("00", timeDifference);
         const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
         const hours = Math.floor(
           (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
