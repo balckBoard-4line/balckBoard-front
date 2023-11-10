a12a3d2f2c45d7c2238cad815d2d0d0e65b2c90f
심서현 <simseohyun@simseohyeon-ui-MacBookAir.local>
2023-11-10 19:58:25 +0900

    :sparkles: FEat: dd

--- src/components/userBlackBoard/BeforeLaunch.jsx
+++ src/components/userBlackBoard/BeforeLaunch.jsx
@@ -62,68 +62,61 @@ function BeforeLaunch({ data, getIsLaunch }) {
   return (
     <div>
       <S.BfWrapper>
-        {loading ? ( // 로딩 중일 때 "Loading..."을 표시
-          <S.Loading>Loading...</S.Loading>
-        ) : (
-          <div>
-            <S.Bfnum1>
-              <S.Bfnum1timewrp>
-                <S.Bfnum1time>
-                  <S.Bfnum1time1>칠판 공개까지</S.Bfnum1time1>
-                  <S.Bfnum1time2>
-                    {`${formatTime(timeLeft.days)} 일 ${formatTime(
-                      timeLeft.hours
-                    )} 시간 ${formatTime(timeLeft.minutes)} 분 ${formatTime(
-                      timeLeft.seconds
-                    )} 초`}
-                  </S.Bfnum1time2>
-                </S.Bfnum1time>
-              </S.Bfnum1timewrp>
-              <S.Bfnum1link>
-                <S.Bfnum1linkwrp>
-                  <img
-                    src="/LinkIcon.png"
-                    alt="Link Icon"
-                    onClick={handleCopyURL}
-                    style={{ cursor: "pointer" }}
-                  />
-                </S.Bfnum1linkwrp>
-              </S.Bfnum1link>
-            </S.Bfnum1>
-            <S.Bfnum2>
-              <S.Bfnum2title>제목</S.Bfnum2title>
-              <br />
-              <S.Bfnum2subtitle>부제</S.Bfnum2subtitle>
-            </S.Bfnum2>
-            <S.Bfnum3>
-              <S.Bfnum3in1wrp>
-                <Link
-                  to={`/letterCreate/${params.url}`}
-                  style={{ width: "100%", height: "100%" }}
-                >
-                  <S.Bfnum3in1>칠판 편지남기기</S.Bfnum3in1>
-                </Link>
-              </S.Bfnum3in1wrp>
-              <S.Bfnum3in2wrp>
-                <Link
-                  to="/visitors/1"
-                  style={{ width: "100%", height: "100%" }}
-                >
-                  <S.Bfnum3in2>다녀간 사람</S.Bfnum3in2>
-                </Link>
-              </S.Bfnum3in2wrp>
-              <S.Bfnum3in3wrp>
-                <Link to="/titleCreate/1">
-                  <S.Bfnum3in3>
-                    <S.Bfnum3in3in1>나도 칠판 만들기</S.Bfnum3in3in1>
-                    <S.Bfnum3in3in2> {">"}</S.Bfnum3in3in2>
-                  </S.Bfnum3in3>
-                </Link>
-              </S.Bfnum3in3wrp>
-            </S.Bfnum3>
-            <S.Bfnum4inbrown />
-          </div>
-        )}
+        <div>
+          <S.Bfnum1>
+            <S.Bfnum1timewrp>
+              <S.Bfnum1time>
+                <S.Bfnum1time1>칠판 공개까지</S.Bfnum1time1>
+                <S.Bfnum1time2>
+                  {`${formatTime(timeLeft.days)} 일 ${formatTime(
+                    timeLeft.hours
+                  )} 시간 ${formatTime(timeLeft.minutes)} 분 ${formatTime(
+                    timeLeft.seconds
+                  )} 초`}
+                </S.Bfnum1time2>
+              </S.Bfnum1time>
+            </S.Bfnum1timewrp>
+            <S.Bfnum1link>
+              <S.Bfnum1linkwrp>
+                <img
+                  src="/LinkIcon.png"
+                  alt="Link Icon"
+                  onClick={handleCopyURL}
+                  style={{ cursor: "pointer" }}
+                />
+              </S.Bfnum1linkwrp>
+            </S.Bfnum1link>
+          </S.Bfnum1>
+          <S.Bfnum2>
+            <S.Bfnum2title>제목</S.Bfnum2title>
+            <br />
+            <S.Bfnum2subtitle>부제</S.Bfnum2subtitle>
+          </S.Bfnum2>
+          <S.Bfnum3>
+            <S.Bfnum3in1wrp>
+              <Link
+                to={`/letterCreate/${params.url}`}
+                style={{ width: "100%", height: "100%" }}
+              >
+                <S.Bfnum3in1>칠판 편지남기기</S.Bfnum3in1>
+              </Link>
+            </S.Bfnum3in1wrp>
+            <S.Bfnum3in2wrp>
+              <Link to="/visitors/1" style={{ width: "100%", height: "100%" }}>
+                <S.Bfnum3in2>다녀간 사람</S.Bfnum3in2>
+              </Link>
+            </S.Bfnum3in2wrp>
+            <S.Bfnum3in3wrp>
+              <Link to="/titleCreate/1">
+                <S.Bfnum3in3>
+                  <S.Bfnum3in3in1>나도 칠판 만들기</S.Bfnum3in3in1>
+                  <S.Bfnum3in3in2> {">"}</S.Bfnum3in3in2>
+                </S.Bfnum3in3>
+              </Link>
+            </S.Bfnum3in3wrp>
+          </S.Bfnum3>
+          <S.Bfnum4inbrown />
+        </div>
       </S.BfWrapper>
     </div>
   );
