dc96f9f152db6c7316fb076f798a8a9f908ef864
dhmr11 <jaco4539@naver.com>
2023-11-11 11:17:15 +0900

    � Feat: BeforeLaunch Loading 수정

--- src/components/userBlackBoard/BeforeLaunch.jsx
+++ src/components/userBlackBoard/BeforeLaunch.jsx
@@ -10,6 +10,7 @@ function BeforeLaunch({ data, getIsLaunch }) {
   const params = useParams();
 
   const emSize = (useInnerWidth() / 375) * 10;
+  const [loading, setLoading] = useState(true);
 
   const [timeLeft, setTimeLeft] = useState({
     day: 0,
@@ -32,12 +33,13 @@ function BeforeLaunch({ data, getIsLaunch }) {
         getIsLaunch(true);
       }
       setTimeLeft({ day: days, hour: hours, minute: minutes, second: seconds });
+      setLoading(false); // 값이 계산되면 로딩을 false로 설정
     }, 1000);
 
     return () => {
       clearInterval(timer);
     };
-  });
+  }, [data.graduateDate, getIsLaunch]);
 
   const handleCopyURL = () => {
     const urlToCopy = window.location.href;
@@ -53,44 +55,52 @@ function BeforeLaunch({ data, getIsLaunch }) {
 
   return (
     <S.PageWrapper>
-      <S.LeftDateWrapper>
-        <S.LeftDateAbout>
-          <S.LeftDateAbout>칠판 공개까지</S.LeftDateAbout>
-          <S.LeftDateContent>
-            {timeLeft.day}일 {timeLeft.hour}시간 {timeLeft.minute}분{" "}
-            {timeLeft.second}초
-          </S.LeftDateContent>
-        </S.LeftDateAbout>
-        <S.CopyBtn src="/LinkIcon.png" onClick={handleCopyURL} />
-      </S.LeftDateWrapper>
+      {loading ? (
+        // 로딩 중인 경우 로딩 표시
+        <S.Loading>Loading...</S.Loading>
+      ) : (
+        // 값이 로드되면 내용을 렌더링
+        <>
+          <S.LeftDateWrapper>
+            <S.LeftDateAbout>
+              <S.LeftDateAbout>칠판 공개까지</S.LeftDateAbout>
+              <S.LeftDateContent>
+                {timeLeft.day}일 {timeLeft.hour}시간 {timeLeft.minute}분{" "}
+                {timeLeft.second}초
+              </S.LeftDateContent>
+            </S.LeftDateAbout>
+            <S.CopyBtn src="/LinkIcon.png" onClick={handleCopyURL} />
+          </S.LeftDateWrapper>
 
-      <S.BlackBoardWrapper $emSize={emSize}>
-        <BlackBoard data={data} type={"title"} />
-      </S.BlackBoardWrapper>
+          <S.BlackBoardWrapper $emSize={emSize}>
+            <BlackBoard data={data} type={"title"} />
+          </S.BlackBoardWrapper>
 
-      <S.FooterWrapper>
-        <Link
-          to={`/letterCreate/${params.url}`}
-          style={{ marginBottom: "5px", width: "100%" }}
-        >
-          <Button content={"칠판 꾸며주기"} type={"white"} />
-        </Link>
-        <Link
-          to={`/visitors/${params.url}`}
-          style={{ marginBottom: "5px", width: "100%" }}
-        >
-          <Button content={"다녀간 사람"} type={"black"} />
-        </Link>
+          <S.FooterWrapper>
+            <Link
+              to={`/letterCreate/${params.url}`}
+              style={{ marginBottom: "5px", width: "100%" }}
+            >
+              <Button content={"칠판 꾸며주기"} type={"white"} />
+            </Link>
+            <Link
+              to={`/visitors/${params.url}`}
+              style={{ marginBottom: "5px", width: "100%" }}
+            >
+              <Button content={"다녀간 사람"} type={"black"} />
+            </Link>
 
-        <Link>
-          <S.WantToMakeBtn>
-            <div>나도 칠판 만들기</div>
-            <span> {">"}</span>
-          </S.WantToMakeBtn>
-        </Link>
+            <Link>
+              <S.WantToMakeBtn>
+                <div>나도 칠판 만들기</div>
+                <span> {">"}</span>
+              </S.WantToMakeBtn>
+            </Link>
 
-        <S.Wood />
-      </S.FooterWrapper>
+            <S.Wood />
+          </S.FooterWrapper>
+        </>
+      )}
     </S.PageWrapper>
   );
 }
